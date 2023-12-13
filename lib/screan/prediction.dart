f91e378f8d1fe2454e5b775791bcd1e00d04e4f9
patpat46 <paetpaet46@gmail.com>
2023-12-13 09:22:28 +0700

    dddd

--- lib/screan/prediction.dart
+++ lib/screan/prediction.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:tflite/tflite.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
 
 class Predict extends StatefulWidget {
   const Predict({super.key});
@@ -229,7 +230,6 @@ class _PredictState extends State<Predict> {
                             ],
                           ),
                   ),
-
                   //predict
                   Column(
                     //image picker
